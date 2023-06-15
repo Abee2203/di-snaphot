35b2e416f4d88c50f227a2dc32ef68483cb6128e
Nayar Joolfoo <nayar@joolfoo.com>
2023-06-15 10:57:15 +0400

    made structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import './App.css';
 
 function App() {
   return (
+    <div>
     <h1>Snapshot</h1>
+    <input type='text' placeholder='Search...'/>
+    <div>
+      <button>Mountain</button>
+      <button>Beaches</button>
+    </div>
+    <div>
+      <h1>Food pictures</h1>
+      <div>
+        <img width='100px' height='100px' src=''/>
+        <img width='100px' height='100px' src=''/>
+        <img width='100px' height='100px' src=''/>
+        <img width='100px' height='100px' src=''/>
+        <img width='100px' height='100px' src=''/>
+        <img width='100px' height='100px' src=''/>
+      </div>
+      
+    </div>
+    </div>
   );
 }
 
