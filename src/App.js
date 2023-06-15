0272a922f9ebebc98ca9e7835e6d91c76ae8d3f5
Hemlesh18 <hemlesh@icloud.com>
2023-06-15 11:44:04 +0400

    kavvind: tags( buttons: mountain,beach)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
+import Tags from './components/tags';
 
 function App() {
   return (
     <div>
+
       <Header title='SnapShot'/>
+
       <input type='text' placeholder='Search...'/>
       <div>
-        <button>Mountain</button>
-        <button>Beaches</button>
+        {/* <Tags title='Mountain'/>
+        <Tags title='Beach'/> */}
+        <Tags />
       </div>
       <div>
         <h1>Food pictures</h1>
---
+++ src/components/tags.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './tags.css'
+class Tags extends React.Component{
+    render(){
+    return <div className='tags'>
+            <button>Mountain</button>
+            <button>Beaches</button>
+          </div>
+}
+
+}
+export default Tags
